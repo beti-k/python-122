d96959064b0371fd41c58bde2d7761f23d309c41
lukatavkhelidze <tavkhelidzeluka@gmail.com>
2025-09-24 16:52:22 +0400

    Add workshop 1 code

---
+++ workshop_1/main.py
@@ -0,0 +1,12 @@
+print("print")
+
+# print("hi" + 5)
+
+print("HI".capitalize())
+print("HIiIiIi".count("i"))
+print("j.r.r tolkien lord of the rings".title())
+print(len("Hi".center(10, "-")))
+
+
+print((4 + 1) == 5)
+print(4 + 1 == 5)
